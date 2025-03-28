4b2a674bae70e6213d3b7bbe1b2dfe77fbf76697
Kahan-CS <kdesai6826@conestogac.on.ca>
2025-03-27 22:45:15 -0400

    Add project files.

---
+++ rt_flight_telemetry_monitor/DateTime.h
@@ -0,0 +1,48 @@
+#pragma once
+class DateTime
+{
+	int day;
+	int month;
+	int year;
+	int hour;
+	int minute;
+	int second;
+
+public:
+	DateTime()
+	{
+		day = 0;
+		month = 0;
+		year = 0;
+		hour = 0;
+		minute = 0;
+		second = 0;
+	}
+	void setDate(int& day, int& month, int& year)
+	{
+		this->day = day;
+		this->month = month;
+		this->year = year;
+	}
+	void getDate(int& day, int& month, int& year)
+	{
+		day = this->day;
+		month = this->month;
+		year = this->year;
+	}
+	void setTime(int& hour, int& minute, int& second)
+	{
+		this->hour = hour;
+		this->minute = minute;
+		this->second = second;
+	}
+
+
+	void getTime(int& hour, int& minute, int& second)
+	{
+		hour = this->hour;
+		minute = this->minute;
+		second = this->second;
+	}
+};
+
---
+++ rt_flight_telemetry_monitor/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+
+
+
+using namespace std;
+
+int main(void)
+{
+
+
+	return 0;
+
+}
